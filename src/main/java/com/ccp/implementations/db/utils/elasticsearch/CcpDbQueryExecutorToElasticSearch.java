6032aaf8aef75376ef816da8488c61553300d1ac
onias <onias@noxxonsat.com.br>
2022-05-22 01:20:46 -0300

    .

--- src/main/java/com/ccp/implementations/db/utils/elasticsearch/CcpDbQueryExecutorToElasticSearch.java
+++ src/main/java/com/ccp/implementations/db/utils/elasticsearch/CcpDbQueryExecutorToElasticSearch.java
@@ -1,26 +1,21 @@
 package com.ccp.implementations.db.utils.elasticsearch;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.function.Consumer;
 
+import com.ccp.constantes.CcpConstantes;
 import com.ccp.decorators.CcpMapDecorator;
 import com.ccp.dependency.injection.CcpEspecification;
 import com.ccp.dependency.injection.CcpImplementation;
 import com.ccp.especifications.db.query.CcpDbQueryExecutor;
 import com.ccp.especifications.db.query.ElasticQuery;
-import com.ccp.especifications.db.utils.CcpDbCredentialsGenerator;
-import com.ccp.especifications.http.CcpHttp;
-import com.ccp.especifications.http.CcpHttpHandler;
-import com.ccp.process.CcpProcess;
+import com.ccp.especifications.db.utils.CcpDbUtils;
 
 @CcpImplementation
 public class CcpDbQueryExecutorToElasticSearch implements CcpDbQueryExecutor {
-	@CcpEspecification
-	private CcpHttp ccpHttp;
 	
 	@CcpEspecification
-	private CcpDbCredentialsGenerator dbCredentials;
+	private CcpDbUtils dbUtils;
 	
 	private final CcpResponseHandlerToSearch handler = new CcpResponseHandlerToSearch();
 
@@ -41,34 +36,22 @@ public class CcpDbQueryExecutorToElasticSearch implements CcpDbQueryExecutor {
 		}
 		return md;
 	}
-	private String getUrl(String[] resourcesNames, String complemento) {
-		return this.dbCredentials.getDatabaseCredentials().getAsString("DB_URL") +  Arrays.asList(resourcesNames).stream().toString()
-				.replace("[", "").replace("]", "").replace(" ", "") + complemento;
-	}
-
 	@Override
 	public CcpMapDecorator delete(ElasticQuery elasticQuery, String[] resourcesNames) {
-		CcpMapDecorator executeHttpRequest = this.executeRequest(elasticQuery, resourcesNames, "/_delete");
-		
-		return executeHttpRequest;
-	}
-	private CcpMapDecorator executeRequest(ElasticQuery elasticQuery, String[] resourcesNames, String complemento) {
-		String url = this.getUrl(resourcesNames, complemento);
-		CcpMapDecorator headers = this.dbCredentials.getDatabaseCredentials();
-		CcpHttpHandler http = new CcpHttpHandler(200, this.ccpHttp);
-		CcpMapDecorator executeHttpRequest = http.executeHttpRequest(url, "POST", headers, elasticQuery.values);
+		CcpMapDecorator executeHttpRequest = this.dbUtils.executeHttpRequest(200, resourcesNames, "/_delete_by_query", "POST", elasticQuery.values);
+
 		return executeHttpRequest;
 	}
 
 	@Override
 	public CcpMapDecorator update(ElasticQuery elasticQuery, String[] resourcesNames, CcpMapDecorator newValues) {
-		CcpMapDecorator executeHttpRequest = this.executeRequest(elasticQuery, resourcesNames, "/_update_by_query");
+		CcpMapDecorator executeHttpRequest = this.dbUtils.executeHttpRequest(200, resourcesNames, "/_update_by_query", "POST", elasticQuery.values);
 		
 		return executeHttpRequest;
 	}
 
 	@Override
-	public void consumeQueryResult(CcpDbQueryExecutor requestExecutor, ElasticQuery elasticQuery, String[] resourcesNames,
+	public void consumeQueryResult(ElasticQuery elasticQuery, String[] resourcesNames,
 			String scrollTime, int size, Consumer<List<CcpMapDecorator>> consumer, String... fields) {
 
 		long total = this.total(elasticQuery, resourcesNames);
@@ -80,21 +63,16 @@ public class CcpDbQueryExecutorToElasticSearch implements CcpDbQueryExecutor {
 			boolean primeiraPagina = k == 0;
 			
 			if(primeiraPagina) {
-				CcpMapDecorator resultAsPackage = elasticQuery.setScrollTime(scrollTime).selectFrom(requestExecutor, resourcesNames).getResultAsPackage(fields);
+				CcpMapDecorator resultAsPackage = this.getResultAsPackage(elasticQuery, resourcesNames, fields);
 				List<CcpMapDecorator> hits = resultAsPackage.getAsMapList("hits");
 				scrollId = resultAsPackage.getAsString("scrollId");
 				consumer.accept(hits);
 				continue;
 			}
 			
-			CcpMapDecorator headers = this.dbCredentials.getDatabaseCredentials();
-			CcpProcess scrollEnded = x -> new CcpMapDecorator();
-			CcpProcess doNothing = x -> x;
-			CcpMapDecorator flows = new CcpMapDecorator().put("200", doNothing).put("404", scrollEnded);
-			CcpHttpHandler http = new CcpHttpHandler(flows, this.ccpHttp);
-			String url = this.dbCredentials.getDatabaseCredentials().getAsString("DB_URL") + "/_search/scroll";
+			CcpMapDecorator flows = new CcpMapDecorator().put("200", CcpConstantes.doNothing).put("404", CcpConstantes.returnEmpty);
 			CcpMapDecorator scrollRequest = new CcpMapDecorator().put("scroll", scrollTime).put("scroll_id", scrollId);
-			CcpMapDecorator executeHttpRequest = http.executeHttpRequest(url, "POST", headers, scrollRequest);
+			CcpMapDecorator executeHttpRequest = this.dbUtils.executeHttpRequest(flows, "/_search/scroll", "POST", scrollRequest);
 			CcpMapDecorator execute = this.handler.execute(executeHttpRequest);
 			List<CcpMapDecorator> hits = execute.getAsMapList("response");
 			consumer.accept(hits);

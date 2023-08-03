8e070cd450ee1d5f2f9975776c6545fd3c3de7f7
onias <onias@noxxonsat.com.br>
2023-08-03 16:49:45 -0300

    .

--- src/main/java/com/ccp/implementations/db/utils/elasticsearch/ResponseHandlerToSearch.java
+++ src/main/java/com/ccp/implementations/db/utils/elasticsearch/ResponseHandlerToSearch.java
@@ -1,26 +1,26 @@
 package com.ccp.implementations.db.utils.elasticsearch;
 
 import java.util.List;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 import com.ccp.decorators.CcpMapDecorator;
 import com.ccp.process.CcpMapTransform;
-import com.ccp.process.CcpProcess;
 
 class ResponseHandlerToSearch implements CcpMapTransform<List<CcpMapDecorator>>{
 	private CcpSourceHandler handler = new CcpSourceHandler();
 	@Override
 	public List<CcpMapDecorator> transform(CcpMapDecorator values) {
 		List<CcpMapDecorator> hits = values.getInternalMap("hits").getAsMapList("hits");
-		List<CcpMapDecorator> collect = hits.stream().map(x -> this.handler.execute(x)).collect(Collectors.toList());
+		List<CcpMapDecorator> collect = hits.stream().map(x -> this.handler.apply(x)).collect(Collectors.toList());
 		return collect;
 	}
 
 }
-class CcpSourceHandler implements CcpProcess{
+class CcpSourceHandler  implements Function<CcpMapDecorator, CcpMapDecorator>{
 
 	@Override
-	public CcpMapDecorator execute(CcpMapDecorator x) {
+	public CcpMapDecorator apply(CcpMapDecorator x) {
 		CcpMapDecorator internalMap = x.getInternalMap("_source");
 		String id = x.getAsString("_id");
 		CcpMapDecorator put = internalMap.put("id", id);

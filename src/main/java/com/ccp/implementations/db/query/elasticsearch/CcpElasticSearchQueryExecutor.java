d1a2f46767cac7ff347e131bd353aad944930e22
ccpjobsnow <69213361+ccpjobsnow@users.noreply.github.com>
2024-04-20 19:41:19 -0300

    .

--- src/main/java/com/ccp/implementations/db/query/elasticsearch/CcpElasticSearchQueryExecutor.java
+++ src/main/java/com/ccp/implementations/db/query/elasticsearch/CcpElasticSearchQueryExecutor.java
@@ -1,11 +1,11 @@
 package com.ccp.implementations.db.query.elasticsearch;
 
 import com.ccp.dependency.injection.CcpInstanceProvider;
+import com.ccp.especifications.db.query.CcpQueryExecutor;
 
-public class CcpElasticSearchQueryExecutor implements CcpInstanceProvider  {
-
+public class CcpElasticSearchQueryExecutor implements CcpInstanceProvider<CcpQueryExecutor>  {
 	
-	public Object getInstance() {
+	public CcpQueryExecutor getInstance() {
 		return new ElasticSearchQueryExecutor();
 	}
 
